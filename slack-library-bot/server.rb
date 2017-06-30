f380666be3b35dcc8b4e47e2d59bd421f3b6da1b
Matt Critchlow <matt.critchlow@gmail.com>
2017-06-30 16:59:23 -0700

    initial formatting for channel_created
    Using a few emojis, because we can. to be continued.

--- slack-library-bot/server.rb
+++ slack-library-bot/server.rb
@@ -1,11 +1,20 @@
 module SlackLibraryBot
   class Server < SlackRubyBot::Server
     on 'channel_created' do |client, data|
-      GENERAL_CHANNEL = 'C04RMMCGX'
-      response =
-        "Hi! A new channel was just created: <##{data['channel']['id']}|#{data['channel']['name']}>"
-      client.say(channel: GENERAL_CHANNEL, text: response)
-      # client.say(channel: 'C62EZ1LLC', text: response)
+      # general_channel = 'C04RMMCGX' general channel
+      general_channel = 'C62EZ1LLC' # bot-testing channel
+
+      new_channel_info = <<~CHANNEL_INFO
+        "Hi! <@#{data['channel']['creator']}> just created a new :slack: channel
+        Check it out :point_right: <##{data['channel']['id']}>"
+      CHANNEL_INFO
+
+      # TODO: this isn't populated. why?
+      if data['channel']['purpose']
+        new_channel_info << "\n Purpose: #{data['channel']['purpose']}"
+      end
+
+      client.say(channel: general_channel, text: new_channel_info)
     end
   end
 end
