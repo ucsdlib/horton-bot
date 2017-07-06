452942a7cf30fa573bb3f910beeb2e82cf3b3ff2
Matt Critchlow <matt.critchlow@gmail.com>
2017-07-06 12:51:00 -0700

    use config file in server test

--- slack-library-bot/server.rb
+++ slack-library-bot/server.rb
@@ -5,14 +5,13 @@ module SlackLibraryBot
   # events from the real-time Slack API
   class Server < SlackRubyBot::Server
     on 'channel_created' do |client, data|
-      general_channel = SlackLibraryBot::Web.settings.channels['general']
-
       new_channel_info = <<~CHANNEL_INFO
         Hi! <@#{data['channel']['creator']}> just created a new :slack: channel
         Check it out :point_right: <##{data['channel']['id']}>
       CHANNEL_INFO
 
-      client.say(channel: general_channel, text: new_channel_info)
+      client.say(channel: SlackLibraryBot::Web.settings.channels['general'],
+                 text: new_channel_info)
     end
   end
 end
