26f3284ed5c655d1dc54fac1aff891c2362d9d76
Matt Critchlow <matt.critchlow@gmail.com>
2018-03-13 10:23:47 -0700

    add more rooms information via an attachment

--- slack-library-bot/commands/rooms.rb
+++ slack-library-bot/commands/rooms.rb
@@ -4,13 +4,31 @@ module SlackLibraryBot
     # This help folks find their way around the Library buildings
     class Rooms < SlackRubyBot::Commands::Base
       command 'rooms' do |client, data, _match|
-        client.say(text: rooms_message,
-                   channel: data.channel)
+        client.web_client.chat_postMessage(
+          channel: data.channel,
+          as_user: true,
+          attachments: [
+            {
+              author_name: 'More information',
+              pretext: rooms_message,
+              title: 'Meeting Rooms, Conference Rooms, and Classrooms',
+              title_link: 'https://lisn.ucsd.edu/pages/viewpage.action?pageId=41914586',
+              text: more_info,
+              color: 'good'
+            }
+          ]
+        )
+      end
+
+      def self.more_info
+        ':information_source: This page on LiSN contains additional'\
+        ' information about former names, administrative contacts,'\
+        ' phone extension, seating capacity, and room technology.'
       end
 
       def self.rooms_message
         rooms_data = SlackLibraryBot::Web.settings.commands['rooms']
-        text = "Hi! Here is a list of our meeting rooms and locations:\n"
+        text = "Hi! Here is a list of our meeting rooms and locations:\n\n"
         text << rooms_data.map do |room|
           "*#{room[:name]}* - #{room[:location]}\n"
         end.join
